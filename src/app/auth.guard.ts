1bfea639947807ddfc94f315dce1b7b87567a7be
whattablackhole <rudolf.gritsenyak@gmail.com>
2024-11-30 05:14:48 +0300

    fix: replace const url

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,9 +1,12 @@
 import { CanActivateFn } from '@angular/router';
+import { environment } from '../environments/environment';
+
+const baseUrl = environment.baseUrl;
 
 export const authGuard: CanActivateFn = (route, state) => {
   const token = localStorage.getItem('access_token');
   if (!token) {
-    window.location.href = 'http://localhost:4000/auth/pipedrive';
+    window.location.href = `${baseUrl}/auth/pipedrive`;
 
     return false;
   }
